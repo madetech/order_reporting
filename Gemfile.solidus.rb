a370754489035cc46be93e77500dd2099c6132b5
Luke <lukemorton.dev@gmail.com>
2016-07-05 14:25:43 +0100

    Split Gemfile into two

---
+++ Gemfile.solidus.rb
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'solidus', github: 'solidusio/solidus'
+gem 'solidus_auth_devise', '~> 1.0'
+gemspec name: 'solidus_order_reporting'
---
+++ Gemfile.spree.rb
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+gem 'spree', github: 'spree/spree', branch: '2-4-stable'
+gem 'spree_auth_devise', github: 'spree/spree_auth_devise', branch: '2-4-stable'
+gemspec name: 'spree_order_reporting'
